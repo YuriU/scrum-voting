db52fbe131b85f80ccbc0cce993b69c18d3a3345
YuriU <jerryhire8@gmail.com>
2020-01-08 18:13:38 +0200

    Added websockets server support

--- backend/handler.js
+++ backend/handler.js
@@ -1,5 +1,30 @@
-module.exports.connectionHandler = async (event, context) => {
+module.exports.connectHandler = async (event, context) => {
   
+    console.log('Websocket connect')
+    console.log(JSON.stringify(event));
+
+    return {
+      statusCode: 200,
+    };
+  };
+
+
+module.exports.disconnectHandler = async (event, context) => {
+  
+    console.log('Websocket disconnect')
+    console.log(JSON.stringify(event));
+
+    return {
+      statusCode: 200,
+    };
+  };
+
+
+module.exports.defaultHandler = async (event, context) => {
+  
+    console.log('Websocket default action')
+    console.log(JSON.stringify(event));
+
     return {
       statusCode: 200,
     };
