5c6da2023b960a7d6b266f3d764ed8911e2ead0a
YuriU <jerryhire8@gmail.com>
2020-01-10 19:24:16 +0200

    Added connection handling

--- backend/handler.js
+++ backend/handler.js
@@ -6,20 +6,34 @@ const DDB = require('./lib/dynamo')
 const tableName = process.env.DDB_TABLE_SESSION;
 
 module.exports.connectHandler = async (event, context) => {
-  
+ 
     console.log('Websocket connect')
     console.log(JSON.stringify(event));
 
     console.log('Endpoint :' + event.requestContext.domainName + "/" + event.requestContext.stage);
     console.log('ConnectionId :' + event.requestContext.connectionId);
-    var key = JSON.stringify({sessionId: 'hello', userId: '123'});
+
+    let sessionKey = {
+      sessionId: 'hello',
+      userId: '123'
+    };
+
+    var headers = event.multiValueHeaders['Sec-WebSocket-Protocol'];
+    if(headers && headers.length > 0){
+      let parts = headers[0].split(',');
+      sessionKey.sessionId = parts[0].trim();
+      sessionKey.userId = parts[1].trim();
+    }
+
+    console.log('Endpoing: ' + JSON.stringify(sessionKey));
 
     var result = await DDB.SessionDB.put({
         Item: {
-          sessionId: 'hello', userId: '123', connectionId: event.requestContext.connectionId
+          sessionId: sessionKey.sessionId, userId: sessionKey.userId, connectionId: event.requestContext.connectionId
         },
         ConditionExpression: 'attribute_exists(sessionId) AND attribute_exists(userId)'
       }).promise();
+
 
     console.log('PutResult: ' + JSON.stringify(result));
 
@@ -30,16 +44,28 @@ module.exports.connectHandler = async (event, context) => {
   };
 
 module.exports.disconnectHandler = async (event, context) => {
-  
+ 
     console.log('Websocket disconnect')
     console.log(JSON.stringify(event));
 
-    var result = await DDB.SessionDB.put({
-      Item: {
-        sessionId: 'hello', userId: '123' //, connectionId: null
-      },
-      ConditionExpression: 'attribute_exists(sessionId) AND attribute_exists(userId)'
+    var sessionAndUserIds = await DDB.SessionDB.query({
+      IndexName: 'gsiConnectionToKeys',
+      KeyConditionExpression: 'connectionId = :con_id',
+      ExpressionAttributeValues: { ':con_id': event.requestContext.connectionId }
     }).promise();
+
+    if(sessionAndUserIds.Items.length > 0){
+
+        var item = sessionAndUserIds.Items[0];
+        console.log(JSON.stringify(item));
+
+        var result = await DDB.SessionDB.put({
+          Item: {
+            sessionId: item.sessionId, userId: item.userId //, connectionId: null
+          },
+          ConditionExpression: 'attribute_exists(sessionId) AND attribute_exists(userId)'
+        }).promise();
+    }
 
     return {
       statusCode: 200,
@@ -48,7 +74,7 @@ module.exports.disconnectHandler = async (event, context) => {
 
 
 module.exports.defaultHandler = async (event, context) => {
-  
+ 
     console.log('Websocket default action')
     console.log(JSON.stringify(event));
 
