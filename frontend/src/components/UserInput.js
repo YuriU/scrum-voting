26f111d5bcad6b2a2851366d6ff62284b9cca21f
YuriU <jerryhire8@gmail.com>
2020-01-12 21:00:53 +0200

    Added names editing

--- frontend/src/components/UserInput.js
+++ frontend/src/components/UserInput.js
@@ -5,17 +5,31 @@ class UserInput extends Component {
         super(props)
 
         console.log(props)
+
         this.state = {
             item: props.item,
             deleteUser: props.deleteUser
         }
+
+        this.handleChangeAlias = this.handleChangeAlias.bind(this);
+        this.handleChangeName = this.handleChangeName.bind(this);
+    }
+
+    handleChangeAlias(event){
+        this.state.item.alias = event.target.value;
+        this.setState(this.state);
+    }
+
+    handleChangeName(event){
+        this.state.item.name = event.target.value;
+        this.setState(this.state);
     }
 
     render(){
         return (
         <div>
-            <label>Alias: <input type="text" value={this.props.item.alias}></input></label>
-            <label>Name: <input type="text" value={this.props.item.name}></input></label>
+            <label>Alias: <input type="text" value={this.props.item.alias} onChange={this.handleChangeAlias}></input></label>
+            <label>Name: <input type="text" value={this.props.item.name} onChange={this.handleChangeName}></input></label>
             <button onClick={(evt) => this.state.deleteUser(this.state.item.userId)}>Delete</button>
         </div>)
     }
