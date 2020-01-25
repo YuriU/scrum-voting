e8a41ab48a470ccad1950576e6a4434e8a31afdb
YuriU <jerryhire8@gmail.com>
2020-01-25 21:24:39 +0200

    Added absolute path for JS bundle

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -5,7 +5,8 @@ module.exports = {
   entry: ['babel-polyfill',"./src/index.js"],
   output: {
     path: path.join(__dirname, "/dist"),
-    filename: "index_bundle.js"
+    filename: "index_bundle.js",
+    publicPath: "/"
   },
   module: {
     rules: [
