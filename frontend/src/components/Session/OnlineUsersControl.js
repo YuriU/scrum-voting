5209027028f91f49c756f0289111418d4090689b
YuriU <jerryhire8@gmail.com>
2020-01-31 14:12:28 +0200

    Extracted online users control to separate component

---
+++ frontend/src/components/Session/OnlineUsersControl.js
@@ -0,0 +1,28 @@
+import React, { Component } from "react";
+import OnlineIndicator from './OnlineIndicator'
+import { copyToClipboard } from '../../utils/clipboard'
+
+class OnlineUsersControl extends Component {
+
+    render() {
+        return (<div className="sessionBoard">
+                { 
+                    this.props.users.map((user, index) => {
+                                                return (<OnlineIndicator 
+                                                            userId={user.userId}
+                                                            text = { user.name }
+                                                            key={user.userId}
+                                                            online={user.online}
+                                                            onClick={(evt) => this.onUserClick(this.props.sessionId, user.userId)}/>)
+                    })
+                }
+                </div>)
+    }
+
+    async onUserClick(sessionId, userId) {
+        let link = `${location.protocol}//${location.host}/vote/${sessionId}/${userId}`;
+        copyToClipboard(link);
+    }
+}
+
+export default OnlineUsersControl;
--- frontend/src/components/Session/SessionScreen.js
+++ frontend/src/components/Session/SessionScreen.js
@@ -1,8 +1,7 @@
 import React, { Component } from "react";
 import _ from 'lodash';
-import { copyToClipboard } from '../../utils/clipboard'
-import OnlineIndicator from './OnlineIndicator'
 import WSClient from '../../api/wsclient'
+import OnlineUsersControl from './OnlineUsersControl'
 import '../../styles/Session.css';
 
 class SessionScreen extends Component {
@@ -31,25 +30,7 @@ class SessionScreen extends Component {
         return (
             <div>
                 <h1>Hello from session {this.props.sessionId}</h1>
-                <div>
-                    <div className="sessionBoard">
-                        { 
-                            this.state.users.map((user, index) => {
-                                const voted = self.state.votedUsers.has(user.userId);
-                                const voteResult = self.state.userVoteResults.has(user.userId) ? self.state.userVoteResults.get(user.userId) : null;
-                                let text = voteResult 
-                                    ? voteResult == '<NA>' ? '-' : `U: ${user.userId}, R: ${voteResult}` 
-                                    : voted ? ' + ' + user.name : user.name;
-                                return (<OnlineIndicator 
-                                                userId={user.userId}
-                                                text = { text }
-                                                key={user.userId}
-                                                online={user.online}
-                                                onClick={(evt) => this.onUserClick(this.props.sessionId, user.userId)}/>)
-                            })
-                        }
-                    </div>
-                </div>
+                <OnlineUsersControl users={this.state.users} sessionId={this.props.sessionId}/>
                 <div>
                     <button onClick={this.onStartVoteClicked}>Start vote</button>
                 </div>
@@ -64,11 +45,6 @@ class SessionScreen extends Component {
             userVoteResults: new Map()
         })
         const result = await this.props.httpClient.startVoting(this.props.sessionId, true);
-    }
-
-    async onUserClick(sessionId, userId) {
-        let link = `${location.protocol}//${location.host}/vote/${sessionId}/${userId}`;
-        copyToClipboard(link);
     }
 
     async componentDidMount() {
