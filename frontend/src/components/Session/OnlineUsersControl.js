ea46ddcafeff0f72893d71cee70062705eaccfb9
YuriU <jerryhire8@gmail.com>
2020-02-04 08:41:39 +0200

    Added piramide like boxed display

--- frontend/src/components/Session/OnlineUsersControl.js
+++ frontend/src/components/Session/OnlineUsersControl.js
@@ -6,18 +6,32 @@ class OnlineUsersControl extends Component {
 
     constructor(props) {
         super(props);
+
+        this.renderRow = this.renderRow.bind(this);
+    }
+
+    renderRow(items, rowIndex) {
+        return (<div key={rowIndex} className="row">
+        {
+            items.map((user, index) => {
+                return (<OnlineIndicator
+                            style= {{margin:'auto'}}
+                            userId = {user.userId}
+                            text = {user.name}
+                            key = {user.userId}
+                            online = {user.online}
+                            onClick={(evt) => this.onUserClick(this.props.sessionId, user.userId)}/>)
+            })
+        }
+        </div>);
     }
 
     render() {
+        const rows = OnlineUsersControl.getBoxesByRows(this.props.users);
         return (<div className="sessionBoard">
                 { 
-                    this.props.users.map((user, index) => {
-                                                return (<OnlineIndicator 
-                                                            userId={user.userId}
-                                                            text = { user.name }
-                                                            key={user.userId}
-                                                            online={user.online}
-                                                            onClick={(evt) => this.onUserClick(this.props.sessionId, user.userId)}/>)
+                    rows.map((row, index) => {
+                       return this.renderRow(row, index)
                     })
                 }
                 </div>)
@@ -26,6 +40,25 @@ class OnlineUsersControl extends Component {
     async onUserClick(sessionId, userId) {
         let link = `${location.protocol}//${location.host}/vote/${sessionId}/${userId}`;
         copyToClipboard(link);
+    }
+
+    static getBoxesByRows(items) {
+        let result = [];
+
+        let rowSize = 1;
+        
+        while(items.length > 0) {
+            let i = 0;
+            const rowItems = [];
+            while(i++ < rowSize && items.length > 0){
+                let item = items.pop();
+                rowItems.push(item)
+            }
+            result.push(rowItems)
+            rowSize++;
+        }
+        
+        return result;
     }
 }
 
