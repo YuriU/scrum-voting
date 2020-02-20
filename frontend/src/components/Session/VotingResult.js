07173a54918fa71c83364959f1a697d8fea7aedd
YuriU <jerryhire8@gmail.com>
2020-02-20 18:15:45 +0200

    Added grouping

--- frontend/src/components/Session/VotingResult.js
+++ frontend/src/components/Session/VotingResult.js
@@ -6,6 +6,9 @@ class VotingResult extends Component {
         super(props);
 
         console.log(JSON.stringify(props.results))
+        let grouped = VotingResult.groupByResult(props.results);
+
+        console.log(JSON.stringify(grouped))
     }
 
     render(){
@@ -21,6 +24,26 @@ class VotingResult extends Component {
                 </div>
             </div>
         )
+    }
+
+    static groupByResult(userIdToResult) {
+
+        const groupedByResult = new Map();
+
+        for (let kvp of userIdToResult) {
+            var userName = kvp[0];
+            var result = kvp[1];
+
+            if(!groupedByResult.has(result)){
+                groupedByResult.set(result, []);
+            }
+
+            console.log(result);
+            var resultValues = groupedByResult.get(result);
+            resultValues.push(userName);
+        }
+
+        return groupedByResult;
     }
 }
 
