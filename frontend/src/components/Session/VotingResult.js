702c61029c470e27058a1400450fe6b7531dd71c
YuriU <jerryhire8@gmail.com>
2020-02-21 17:06:41 +0200

    Added results output

--- frontend/src/components/Session/VotingResult.js
+++ frontend/src/components/Session/VotingResult.js
@@ -7,18 +7,25 @@ class VotingResult extends Component {
 
         console.log(JSON.stringify(props.results))
         let grouped = VotingResult.groupByResult(props.results);
+        this.state ={
+            groupedByResult : grouped
+        };
 
         console.log(JSON.stringify(grouped))
     }
 
     render(){
+
+        console.log(this.state)
         return (
             <div>
                 <div>
                     <h1>Voting result</h1>
                     {
-                        Array.from(this.props.results.entries()).map((res, index) => {
-                        return (<div key={index}><h1>{res[0]} : {res[1]}</h1></div>)
+                        Array.from(this.state.groupedByResult).map((res, index) => {
+                        return (<div key={index}><h1>{res[0]} : {res[1].map((userId, index) => {
+                            return (<div>{userId}</div>)
+                        })}</h1></div>)
                         })
                     }
                 </div>
