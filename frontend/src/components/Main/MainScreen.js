7c220569f733175ca57a0ddf9d5af63790c0afb5
YuriU <jerryhire8@gmail.com>
2020-02-26 22:00:31 +0200

    Added check for login status

--- frontend/src/components/Main/MainScreen.js
+++ frontend/src/components/Main/MainScreen.js
@@ -1,23 +1,54 @@
-import React, { Component } from "react";
-import { Link } from "react-router-dom";
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import { Auth } from 'aws-amplify';
+
 class MainScreen extends Component {
 
+    constructor(props) {
+      super(props);
+
+      this.state = {
+        authenticated: false
+      };
+    }
+
+
+    async componentDidMount() {
+      try{
+        const currentSession = await Auth.currentSession();
+        console.log(currentSession);
+        if(currentSession) {
+          this.setState({
+            authenticated: true
+          })
+        }
+      }
+      catch(err){
+        console.log(err)
+      }
+    }
+
     render() {
+      if(this.state.authenticated) {
         return (<div>
-            <nav>
-              <ul>
-                <li>
-                  <Link to="/">Home</Link>
-                </li>
-                <li>
-                  <Link to="/startSession">Start Session</Link>
-                </li>
-                <li>
-                  <Link to="/session">Session</Link>
-                </li>
-              </ul>
-            </nav>
-          </div>)
+          <nav>
+            <ul>
+              <li>
+                <Link to="/">Home</Link>
+              </li>
+              <li>
+                <Link to="/startSession">Start Session</Link>
+              </li>
+              <li>
+                <Link to="/session">Session</Link>
+              </li>
+            </ul>
+          </nav>
+        </div>)
+      }
+      else {
+        return (<div>Login please</div>)
+      }
     }
 }
 
