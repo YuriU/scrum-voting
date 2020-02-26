f6de8ba899b144cde7dff515c0e1de7a331d1b2a
YuriU <jerryhire8@gmail.com>
2020-02-26 09:40:26 +0200

    Added amplifyjs integration

--- scripts/build_frontend_config.sh
+++ scripts/build_frontend_config.sh
@@ -29,16 +29,24 @@ USER_POOL_ID=$(aws \
     --query "Stacks[0].Outputs[?OutputKey=='UserPoolId'] | [0].OutputValue" \
     --output text)
 
+USER_POOL_CLIENT_ID=$(aws \
+    cloudformation describe-stacks \
+    --stack-name "scrum-vote-${STAGE}" \
+    --query "Stacks[0].Outputs[?OutputKey=='UserPoolAppClientId'] | [0].OutputValue" \
+    --output text)
+
 echo ${HTTP_ENDPOINT}
 echo ${WS_ENDPOINT}
 echo ${REGION}
 echo ${USER_POOL_ID}
+echo ${USER_POOL_CLIENT_ID}
 
 echo "const Config = {
     Region: \"${REGION}\",
     BackendHttpEndpoint: \"${HTTP_ENDPOINT}\",
     BackendWebSocketEndpoint: \"${WS_ENDPOINT}\",
-    UserPoolId: \"${USER_POOL_ID}\"
+    UserPoolId: \"${USER_POOL_ID}\",
+    UserPoolClientId: \"${USER_POOL_CLIENT_ID}\"
 }
 
 export default Config;" > ${DESTINATION}
