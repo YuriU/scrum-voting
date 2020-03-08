093d06c99658337bd0bcd1a84cfd3854329075a8
YuriU <jerryhire8@gmail.com>
2020-03-08 18:36:24 +0200

    Fixed small issues with the script

--- createuser.sh
+++ createuser.sh
@@ -1,5 +1,5 @@
 aws cognito-idp admin-create-user \
-    --user-pool-id us-west-2_aaaaaaaaa \
-    --username diego@example.com \
-    --user-attributes Name=email,Value=kermit2@somewhere.com Name=phone_number,Value="+15555551212" \
+    --user-pool-id <USERPOOLID> \
+    --username <USERID> \
+    --temporary-password <PASSWORD> \
     --message-action SUPPRESS
