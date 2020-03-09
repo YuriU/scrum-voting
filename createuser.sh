b4a9f1b14dddc926911aa7e27da495ea5ad718f8
YuriU <jerryhire8@gmail.com>
2020-03-09 21:34:33 +0200

    Make create user command more wise

--- createuser.sh
+++ createuser.sh
@@ -1,5 +1,17 @@
+#!/bin/bash
+set -eu
+
+STAGE="${1:-dev}"
+USER_NAME=$2
+
+USER_POOL_ID=$(aws \
+    cloudformation describe-stacks \
+    --stack-name "scrum-vote-${STAGE}" \
+    --query "Stacks[0].Outputs[?OutputKey=='UserPoolId'] | [0].OutputValue" \
+    --output text)
+
 aws cognito-idp admin-create-user \
-    --user-pool-id <USERPOOLID> \
-    --username <USERID> \
-    --temporary-password <PASSWORD> \
+    --user-pool-id $USER_POOL_ID \
+    --username $USER_NAME \
+    --temporary-password MyTestPassword!2020 \
     --message-action SUPPRESS
